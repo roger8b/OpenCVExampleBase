36bdd973e697b347764a2487e35f4b93d865f8ad
Roger Martins <roger.martins@madeinweb.com.br>
2018-10-08 00:24:42 -0300

    Implantado exemplo da camera

--- app/src/main/java/com/example/rm/opencvexample/MainActivity.java
+++ app/src/main/java/com/example/rm/opencvexample/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.rm.opencvexample;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,9 +19,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        // Button to call OpenCV Camera Activity
+        Button cameraInit = (Button) findViewById(R.id.cameraInit);
+        cameraInit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),OpenCVCamera.class);
+                startActivity(i);
+            }
+        });
     }
 
     /**
